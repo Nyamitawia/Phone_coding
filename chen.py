feb1c4a0fb8c2c72131ea376b0efc88ff2031530
Nyamitawia <nyamitawiabelua@gmail.com>
2024-09-20 03:17:19 +0300

    This is my second mobile commit

---
+++ chen.py
@@ -0,0 +1,11 @@
+grade=int(input("Enter your grade:"))
+if grade>=90:
+   print("Grade A")
+elif grade>=80:
+   print("Grade B")
+elif grade>=70:
+   print("Grade c")
+elif grade>=60:
+   print("Grade D")
+else:
+   print("Grade F")
